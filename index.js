4cc6144b8fd63f96a3fd9f1984a9b7c353f1cbbf
Talisha White <misstalishawhite@gmail.com>
2021-06-22 21:20:18 +0000

    added get routes

--- index.js
+++ index.js
@@ -23,9 +23,59 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('handlebars', exphbs({ defaultLayout: "main" }));
 app.set('view engine', 'handlebars');
 
+//GET ROUTES
 app.get('/', (req, res) => {
-    res.render('home', { msg: "Royal International Miss 2021" });
+    res.render('splash')
 });
+
+app.get('/register', (req, res) =>{
+  res.render('register')
+})
+
+app.get('/password-reset', (req, res) => {
+  res.render('password-reset')
+})
+
+app.get('/home', (req, res) => {
+  res.render('home')
+})
+
+app.get('/feed', (req, res) => {
+  res.render('feed')
+})
+
+app.get('/profile/:id', (req, res) =>{
+  res.render('profile')
+})
+
+app.get('/announcements', (req, res) => {
+  res.render('announcements')
+})
+
+app.get('/chat', (req, res) => {
+  res.render('chat')
+})
+
+app.get('/chat/:id', (req, res) => {
+  res.render('chat-message')
+})
+
+app.get('/agenda', (req, res) => {
+  res.render('agenda')
+})
+
+app.get('/faqs', (req, res) => {
+  res.render('faqs')
+})
+
+app.get('/resources', (req, res) => {
+  res.render('resources')
+})
+
+app.get('/agenda/:id', (req, res) => {
+  res.render('agenda-personal')
+})
+// POST ROUTES
 app.post('/event', (req, res) => {
     console.log(req.body)
     Event.create(req.body).then((event) => {
@@ -34,6 +84,11 @@ app.post('/event', (req, res) => {
     }).catch((err) => {
 	console.log(err.message);
     })
+})
+
+app.post('/login', (req, res) => {
+  console.log(req.body)
+  res.redirect('home')
 })
 
 app.listen(3000, () => {
